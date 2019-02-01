2f97d0717a63eb6f6b37c2658462d5afb92512d2
Mohamed20222 <44948619+Mohamed20222@users.noreply.github.com>
2019-02-01 23:28:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "y";
+const prefix = "M";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`yplay | YouTube`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Mplay | Mina`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
